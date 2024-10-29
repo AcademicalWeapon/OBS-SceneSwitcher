3cee4a1cc92c36ce76141fb305d7dc2b5520fad7
Ivelin Ivanov <ivelin.ivanov209@gmail.com>
2024-10-29 20:17:03 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -3,51 +3,13 @@
 import os
 import cv2
 import speech_recognition as sr
-from dotenv import load_dotenv
-from obswebsocket import obsws, requests
+import tkinter as tk
+import customtkinter
 
-load_dotenv()
-
-# Set up connection parameters
-host: str = "localhost"
-port: int = 4444
-password: str = os.getenv('api_key')
-
-
-# OBS Cred
-ws = obsws(host, port, password)
-
-def ConnectOBS() -> None:
-    """Connects to OBS"""
-    try:
-        ws.connect()
-        print("Connected")
-    except Exception as e:
-        print(f"Error Occured while trying to Connect: {e}")
-
-
-def SwitchScene(scene) -> None:
-    """Switches to given Scene"""
-    try:
-        StandbyScene: int = scene
-        ws.call(requests.SetCurrentScene(**{'scene-name': StandbyScene})) # WHY???
-        print("Scene Switched")
-    except Exception as e:
-        print(f"Error Occured while trying to Switch Scenes: {e}")
-
-
-def DisconnectOBS() -> None:
-    """Disconnects from OBS"""
-    try:
-        ws.disconnect()
-        print("Disconnected")
-    except Exception as e:
-        print(f"Error Occured while trying to Disconnect: {e}")
-
+# NOTE: The GUI is being developed by @AcademicalWeapon on his PC since cba to create a codespace
 
 r = sr.Recognizer()
 with sr.Microphone() as source:
-    print("Say something!")
     audio = r.listen(source)
 
 try:
