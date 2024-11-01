c89b04ddda33f9ecaa440ef2735f3e7ae4d5d1f0
AcademicalWeapon <ivelin.ivanov209@gmail.com>
2024-11-01 15:13:41 +0100

    Added some things to main.py

--- main.py
+++ main.py
@@ -2,6 +2,14 @@
 
 import os
 import tkinter as tk
-import customtkinter
+import customtkinter as ctk
+import SceneSwitch as SS
+import AudioImageDet as AID
 
 # NOTE: The GUI is being developed by @AcademicalWeapon on his PC since cba to create a codespace
+
+# Loads the Keywords File
+KEYWORDS = AID.LoadKeywords("keywords.txt")
+
+# Start the Audio Detection
+AID.AudioDetection(KEYWORDS)
